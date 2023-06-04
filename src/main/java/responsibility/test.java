dc66959f247422c6c1373048eefe45f504db3567
TianHui <970921208@qq.com>
2023-06-04 22:27:11 +0800

    增加注释

--- src/main/java/responsibility/test.java
+++ src/main/java/responsibility/test.java
@@ -2,8 +2,11 @@ package responsibility;
 
 public class test {
     public static void main(String[] args) {
+        // 非空检测
         Handler emptyHandler = new EmptyHandler();
+        // 特殊字符检测
         Handler specialCharacterHandler = new SpecialCharacterHandler();
+        // 全局检测
         Handler globalEventHandler = new GlobalEventHandler();
         globalEventHandler.setSuccessor(emptyHandler);
         emptyHandler.setSuccessor(specialCharacterHandler);
